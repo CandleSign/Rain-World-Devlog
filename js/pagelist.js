e749d07f0178bca4c3972679bff470b005f1869a
CandleSign <107418075+CandleSign@users.noreply.github.com>
2023-01-02 00:31:22 +0100

    Create pagelist.js

---
+++ js/pagelist.js
@@ -0,0 +1,44 @@
+if (document.getElementById('pagelist')) {
+	var pl = [];
+	var pn = Number(window.location.pathname.split('/').pop().split('.').shift());
+	for (var j=pn-5; j < pn+5; j++) {
+		if (j > 0 && j < 96) {
+			pl.push(j);
+					};
+	};
+	if (pl[0] > 2) {
+		pl.unshift(1, '...');
+	} else if (pl[0] > 1) {
+		pl.unshift(1);
+	};
+	if (pl[0] < 94) {
+		pl.push('...', 95);
+	} else if (pl[0] < 95) {
+		pl.push(95);
+	};
+	var pl2 = [];
+	pl.forEach(
+		function (v, i) {
+			var endtxt1 = v;
+			if (v != '...') {
+				var endtxt1 = v;
+				if (Number(v) == pn) {
+					endtxt1 = '[' + endtxt1 + ']';
+				} else {
+					endtxt1 = '<a href="/Rain-World-Devlog/Pages/00' + v + '.html">' + endtxt1 + '</a>';
+				};
+			};
+			pl2[i] = endtxt1;
+		}
+	);
+	var endtxt2 = pl2.join(' ');
+	if (pn > 1) {
+		var n = pn-1
+		endtxt2 = '<a href="/Rain-World-Devlog/Pages/00' + n + '.html"><</a> ' + endtxt2;
+	};
+	if (pn < 95) {
+		var n = pn+1
+		endtxt2 = endtxt2 + ' <a href="/Rain-World-Devlog/Pages/00' + n + '.html">></a>'
+	};
+	document.getElementById('pagelist').innerHTML = endtxt2;
+}
